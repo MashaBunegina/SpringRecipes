51e160504c16a38e07cfecfac2a86a89c9d4d120
Maria <maria.bunegina@mail.ru>
2023-01-10 23:07:52 +0300

    .

--- src/main/java/me/buneginam/recipesapp/controllers/FirstController.java
+++ src/main/java/me/buneginam/recipesapp/controllers/FirstController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FirstController {
     @GetMapping("/")
     public String helloRecipes() {
-        return "Привет. Это приложение с рецептами";
+        return "Привет.Это приложение с рецептами";
     }
 
     @GetMapping("/info")
